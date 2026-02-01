475cb45c724a0835953edfa0aefb9b4453b993a1
yuutsu <nurikbaidibek@gmail.com>
2026-02-01 13:13:46 +0500

    refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ chapters_text = get_chapters_text_from_file()
 choice = random.choice(chapters_list)
 
 random_chapter_paragraphs = chapters_text[choice.volume][choice.number].get("content")
-idx = random.randrange(len(random_chapter_paragraphs))
+idx = random.randrange(len(random_chapter_paragraphs) - 1)
 random_paragraph = random_chapter_paragraphs[idx]
 print(choice.name ,choice.volume,choice.number,idx,random_paragraph.get("content").get("text"))
 #initer_text = InitChaptersText(Config.url_base_for_get_chapter)
@@ -22,5 +22,5 @@ analyzer_object = Analyzer(chapters_text)
 #analyzer_object.print_count_paragraphs_and_count_words()
 
 
-text = "Он понимал насколько силен Рудра, но не предполагал, что тот сможет победить его теперь, когда была Стража Замка."
-#analyzer_object.search_text(text)
+text = "Мы вернулись примерно через час, следуя по проложенной нами тропе. Я воспользовался Мысленным Общением, чтобы предупредить Гельда, так что по прибытии нам приготовили еду. Трое моих спутников не могли выглядеть более смущенными."
+analyzer_object.search_text(text)
