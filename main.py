fcd12599da6f88c0411c5763e7f2a8a88000bf29
yuutsu <nurikbaidibek@gmail.com>
2026-02-01 13:06:37 +0500

    refactor

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from analyzer import Analyzer
 chapters_list = get_chapters_id_from_file()
 chapters_text = get_chapters_text_from_file()
 choice = random.choice(chapters_list)
-print(choice.name ,choice.volume,choice.number)
+
 random_chapter_paragraphs = chapters_text[choice.volume][choice.number].get("content")
 idx = random.randrange(len(random_chapter_paragraphs))
 random_paragraph = random_chapter_paragraphs[idx]
-print(idx,random_paragraph.get("content").get("text"))
+print(choice.name ,choice.volume,choice.number,idx,random_paragraph.get("content").get("text"))
 #initer_text = InitChaptersText(Config.url_base_for_get_chapter)
 #initer_text.save(chapters_list)
 
