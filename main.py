aeb68b12d2e983d76b6cc1f4e804536462da3667
yuutsu <nurikbaidibek@gmail.com>
2026-04-16 01:00:38 +0500

    update ayanokoji

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from init_chapters_text import InitChaptersText
 from analyzer import Analyzer
 
 chapters_list = get_chapters_id_from_file()
-#chapters_text = get_chapters_text_from_file()
+chapters_text = get_chapters_text_from_file()
 
 #choice = random.choice(chapters_list)
 #random_chapter_paragraphs = chapters_text[choice.volume][choice.number].get("content")
 #idx = random.randrange(len(random_chapter_paragraphs))
 #random_paragraph = random_chapter_paragraphs[idx]
 #print(choice.name ,choice.volume,choice.number,idx,random_paragraph.get("content").get("text"))
-initer_text = InitChaptersText(Config.url_base_for_get_chapter)
-initer_text.save(chapters_list)
+# initer_text = InitChaptersText(Config.url_base_for_get_chapter)
+# initer_text.save(chapters_list)
 
-#analyzer_object = Analyzer(chapters_text)
+analyzer_object = Analyzer(chapters_text)
 
-#analyzer_object.print_volume_count_and_chapters_length()
-#analyzer_object.print_count_paragraphs_and_count_words()
+analyzer_object.print_volume_count_and_chapters_length()
+analyzer_object.print_count_paragraphs_and_count_words()
 
 
 # text = "Вестер"
